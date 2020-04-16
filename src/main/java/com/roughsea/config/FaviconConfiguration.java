bdf5d9e63761ef6266d77c7fe47acbaf6d7dc33d
Danila <karnacevich5323537@gmail.com>
2020-04-16 20:14:11 +0300

    FAVICON

---
+++ src/main/java/com/roughsea/config/FaviconConfiguration.java
@@ -0,0 +1,34 @@
+package com.roughsea.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.io.ClassPathResource;
+import org.springframework.core.io.Resource;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.servlet.handler.SimpleUrlHandlerMapping;
+import org.springframework.web.servlet.resource.ResourceHttpRequestHandler;
+
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+
+@Configuration
+public class FaviconConfiguration {
+
+    @Bean
+    public SimpleUrlHandlerMapping myFaviconHandlerMapping() {
+        SimpleUrlHandlerMapping mapping = new SimpleUrlHandlerMapping();
+        mapping.setOrder(Integer.MIN_VALUE);
+        mapping.setUrlMap(Collections.singletonMap("src/resources/favicon.ico", faviconRequestHandler()));
+        return mapping;
+    }
+
+    @Bean
+    protected ResourceHttpRequestHandler faviconRequestHandler() {
+        ResourceHttpRequestHandler requestHandler = new ResourceHttpRequestHandler();
+        requestHandler.setLocations(Arrays.asList(new ClassPathResource("/")));
+        return requestHandler;
+    }
+}
